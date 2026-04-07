eac0758c0e600dddf4ec9433feff06c42e54aaec
Nishant Kumar <vijayporwal09@gmail.com>
2026-04-07 10:17:11 +0530

    added more topics thta i covered today

---
+++ destructuring.js
@@ -0,0 +1,6 @@
+// destructuring = extract values from arrays and objects,
+//                 then assing them to variables in a convenient way 
+//                 [] = to perform araay destructuring
+//                 {} = tp perfrom object destructuring
+
+//
--- getter&setter.js
+++ getter&setter.js
@@ -26,13 +26,13 @@ class Rectangle{
     }
 // getters
     get width(){
-        return this._width;
+        return `${this._width.toFixed(1)}cm`;
     }
     get height(){
-        return this._height;
+        return `${this._height.toFixed(1)}cm`;
     }
     get area(){
-        return this._width * this._height;
+        return `${this._width * this._height.toFixed(1)}cm`;
     }
 }
 
@@ -45,3 +45,52 @@ rectangle.height = 2;
 console.log(rectangle.width);
 console.log(rectangle.height);
 console.log(rectangle.area);
+
+// Example 02
+class Person{
+    constructor(firstName, lastName, age){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+
+    }
+    set firstName(newfristName){
+        if(typeof newfristName ==="string" && newfristName.length > 0){
+            this._firstName = newfristName;
+
+        }
+        else{
+            console.error(" First name must be a non-empty string");
+        }
+            
+    }
+    set lastName(newlastName){
+        if(typeof newlastName === "string" && newlastName.length > 0){
+            this._lastName = newlastName;
+        }
+        else{
+            console.error("Last name must be a non-empty string ")
+        }
+    }
+    set age(newAge){
+        if(typeof newAge === "number" && newAge.length > 0){
+            this._age = newAge;
+        }
+        else{
+            console.error("Age must be a non-negative number")
+        }
+    }
+    get firstName(){
+        return this._firstName
+    }
+    get lastName(){
+        return this._lastName
+    }
+    get age(){
+        return this._age
+    }
+}
+const person = new Person("Nishant", "Kumar",20);
+console.log(person.firstName);
+console.log(person.lastName);
+console.log(person.age);
